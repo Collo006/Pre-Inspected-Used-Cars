3ccf120e3ca80d0176802fbd68dcc5044eb68c1f
Collins Kipruto <collinskipruto036@gmail.com>
2025-10-26 01:11:31 +0300

    still working on the sliding effect

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -8,3 +8,18 @@ export type Car = {
 export type CarsData={
     cars:Car[]
 }
+
+export type NewArrival = {
+    id:string;
+    model:string;
+    location:string;
+    image:string;
+    price:string;
+    mileage:string;
+    year:string;
+    engine_size:string;
+};
+
+export type NewArrivalData = {
+    newarrivals: NewArrival[]
+}
--- client/src/layout/LandingPage.tsx
+++ client/src/layout/LandingPage.tsx
@@ -1,14 +1,95 @@
 import { useQuery } from "@apollo/client/react";
 import { gql } from "@apollo/client";
-import { Car,CarsData } from "../interfaces";
+import { NewArrival,NewArrivalData } from "../interfaces";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
+const GET_NEW_ARRIVALS = gql `query CarsQuery {
+  newarrivals{
+    id
+    model
+    location
+    image
+    price
+    mileage
+    year
+    engine_size
+  }
+}
+`
 
 
 export default function LandingPage() {
-return(
-    <section>
 
+const{data, error, loading}= useQuery<NewArrivalData>(GET_NEW_ARRIVALS)
+
+const [currentIndex,setCurrentIndex]= useState(0)
+
+useEffect(()=>{
+
+    if(!data?.newarrivals || data.newarrivals.length === 0)return ;
+
+   const total = data.newarrivals.length;
+
+    const interval = setInterval(()=>{
+        setCurrentIndex((prev)=>  
+            prev === total - 1 ? 0:prev +1
+    );
+    }, 4000);
+
+    return ()=> clearInterval(interval);
+}, [data?.newarrivals]);
+
+if(loading)return <p>Data Loading...</p>
+if(error)return <p>Error...{error.message}</p>
+if(!data?.newarrivals)return <p>No cars to Display</p>
+
+
+
+return(
+  <div>
+    <div className="mt-2">
+    <h1>NEW ARRIVALS</h1>
+    <p>Find Newly Posted Cars from approved sellers</p>
+    </div>
+    <section className="mt-2 w-full flex justify-center ">
+    <div className="relative grid grid-cols-2 items-center w-full h-[400px]">
+        {/**Left image */}
+        <div className="relative w-full h-full border border-red-600 ">
+          <Image src={data.newarrivals[currentIndex].image} alt={data.newarrivals[currentIndex].model} fill className="object-cover rounded-l-lg transition-all duration-700"/>
+        </div>
+        {/*Right Details*/ }
+        <div className="flex flex-col justify-center p-6 bg-white h-full transition-all duration-700 border border-amber-600">
+         {/*<h2 className="text-2xl font-semibold mb-2">
+            {data.newarrivals[currentIndex].model}
+         </h2>*/}
+         <div className="-mt-60 text-center  grid grid-cols-2">
+          <p className="ml-58 text-center pl-12 text-sm w-auto pt-2">Price</p>
+          <p className="-ml-72 text-center"><span className="text-6xl">{data.newarrivals[currentIndex].price}</span></p>
+         </div>
+         <div className="border border-green-600 grid grid-cols-2">
+<p> Year: {data.newarrivals[currentIndex].year}</p>
+        <p> Model: {data.newarrivals[currentIndex].model}</p>
+        <p> Mileage: {data.newarrivals[currentIndex].mileage}</p>
+        <p> Engine Size: {data.newarrivals[currentIndex].engine_size}</p>
+       
+         </div>
+{/*<p> Location: {data.newarrivals[currentIndex].location}</p>
+        <p> Price: {data.newarrivals[currentIndex].price}</p>
+        <p> Mileage: {data.newarrivals[currentIndex].mileage}</p>
+        <p> Engine Size: {data.newarrivals[currentIndex].engine_size}</p>
+        <p> Year: {data.newarrivals[currentIndex].year}</p>*/}
+        </div>
+    {/**Dots Indicator */}
+    <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 flex gap-2">
+     {data.newarrivals.map((_,index)=>(
+        <span key={index} className={`h-2 w-2 rounded-full transition-all duration-500 ${index === currentIndex ? "bg-black w-4":"bg-gray-400"}`}></span>
+     ))}
+    </div>
+    </div>
+
+       
     </section>
+    </div>
 )
 }
