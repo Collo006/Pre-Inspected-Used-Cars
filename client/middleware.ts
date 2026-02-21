7a30bcd94edd39dc2fe9acdff2560484bcdde65b
Collins Kipruto <collinskipruto036@gmail.com>
2026-02-21 09:07:11 +0300

    Refactor middleware to clean up protected paths and improve code readability

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,37 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
 
-//protected paths that reqiure authentication
-const protectedPaths=[
-    '/Buy',
-    '/About',
-    '/SellCarForm',
-    '/SignUp'
-]
+// protected paths
+const protectedPaths = [
+  "/Buy",
+  "/About",
+  "/SellCarForm",
+];
 
-export async function middleware(request:NextRequest){
-  
-    const pathname= request.nextUrl.pathname
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
 
-    //check if the current path is in protectedPaths
-    const isProtectedPath= protectedPaths.some(path=>pathname.startsWith(path))
+  const isProtectedPath = protectedPaths.some((path) =>
+    pathname.startsWith(path)
+  );
 
- // check if session cookie exists
+  // check if session cookie exists
   const sessionCookie = request.cookies.get("better-auth.session_token");
 
-    //if it's a protcted path and no session exists,redirect to sign-in
-    if(isProtectedPath && !sessionCookie){
-        const signInUrl=new URL('/SignUp',request.url)
-        signInUrl.searchParams.set('callbackUrl',pathname)
-        return NextResponse.redirect(signInUrl)
-    }
-    return NextResponse.next()
+  if (isProtectedPath && !sessionCookie) {
+    const signInUrl = new URL("/SignUp", request.url);
+    signInUrl.searchParams.set("callbackUrl", pathname);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return NextResponse.next();
 }
-//configure which routes to run middleware on
-export const config ={
-    matcher:[
-      '/Buy',
-    '/About',
-    '/SellCarForm',
-    '/SignUp'
-    ]
-}
+
+export const config = {
+  matcher: ["/Buy", "/About", "/SellCarForm"],
+};
