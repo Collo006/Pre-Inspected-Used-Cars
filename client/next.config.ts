9a208ceceb16ee497f1204068558b360f8779147
Collins Kipruto <collinskipruto036@gmail.com>
2026-02-21 11:36:28 +0300

    refactor: remove experimental configuration and streamline nextConfig export

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,19 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  experimental: {
-    appDir: true,   // this is usually true by default in Next 14+
-    // optionally:
-    // root: 'src'   <-- Next.js currently doesn’t support changing root, so better move app folder
-  },
-  // Or disable only for Prisma
   images: {
     domains: ['localhost'],
     unoptimized: true, // If you have many images
   },
 }
-module.exports = {
-  trailingSlash: true,
-  nextConfig
-   // or false, but be consistent
-};
+module.exports = nextConfig
+  
