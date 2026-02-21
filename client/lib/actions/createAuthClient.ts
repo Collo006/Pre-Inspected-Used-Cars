4f2cfa97aba644737a0091da8fb13971c8b2c521
Collins Kipruto <collinskipruto036@gmail.com>
2026-02-21 08:26:59 +0300

    Refactor auth client configuration to use env for baseURL

--- client/lib/actions/createAuthClient.ts
+++ client/lib/actions/createAuthClient.ts
@@ -1,7 +1,9 @@
 //used to create the useSession State since better auth doesn't have it in it's library
 
 import { createAuthClient } from "better-auth/react";
+import { env } from "process";
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL, 
+  baseURL: env.NEXT_PUBLIC_BETTER_AUTH_URL, 
 });
 export const { useSession, getSession, signIn, signOut } = authClient;
