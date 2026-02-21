53c898eea2ed4bf39be3606eb25a72f4503aeaf9
Collins Kipruto <collinskipruto036@gmail.com>
2026-02-21 11:25:05 +0300

    fix: update package dependencies and adjust import paths
    - Downgraded @eslint/eslintrc to version 0.1.0 and adjusted eslint and eslint-config-next versions for compatibility.
    - Changed import path for Apollo client in ApolloWrapper component to reflect new directory structure.
    - Removed old middleware.ts file and created a new one in the correct directory with the same functionality for protected routes.

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  
+  experimental: {
+    appDir: true,   // this is usually true by default in Next 14+
+    // optionally:
+    // root: 'src'   <-- Next.js currently doesn’t support changing root, so better move app folder
+  },
   // Or disable only for Prisma
   images: {
     domains: ['localhost'],
--- client/src/components/ApolloWrapper.tsx
+++ client/src/components/ApolloWrapper.tsx
@@ -1,5 +1,5 @@
 "use client"
-import client from "@/app/lib/apollo-client";
+import client from "../app/lib/apollo-client";
 import { ApolloProvider } from "@apollo/client/react";
 
  // tells the server to change this to a client
