4ff20f83c28c253574030227a540867b9314994c
Collins Kipruto <collinskipruto036@gmail.com>
2026-02-27 10:27:56 +0300

    Removed the second logo item

--- client/middleware.ts
+++ client/middleware.ts
@@ -9,26 +9,22 @@ const protectedPaths = [
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   
-  // ADD THIS DEBUG LOG
-  console.log("🔍 MIDDLEWARE RUNNING ON:", pathname);
-  console.log("🍪 SESSION COOKIE:", request.cookies.get("better-auth.session_token") ? "EXISTS" : "MISSING");
-  
   const isProtectedPath = protectedPaths.some((path) =>
     pathname.startsWith(path)
   );
   
-  console.log("🛡️ IS PROTECTED PATH:", isProtectedPath);
+  console.log(" IS PROTECTED PATH:", isProtectedPath);
 
   const sessionCookie = request.cookies.get("better-auth.session_token");
 
   if (isProtectedPath && !sessionCookie && pathname !=='/') {
-    console.log("🚫 REDIRECTING TO SIGNUP FROM:", pathname);
+    console.log(" REDIRECTING TO SIGNUP FROM:", pathname);
     const signInUrl = new URL("/SignUp", request.url);
     signInUrl.searchParams.set("callbackUrl", pathname);
     return NextResponse.redirect(signInUrl);
   }
 
-  console.log("✅ ALLOWING ACCESS TO:", pathname);
+  console.log(" ALLOWING ACCESS TO:", pathname);
   return NextResponse.next();
 }
 
